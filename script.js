36bb2450b2e23c1b8c8ff8e37728d99f96cc6a30
FranV <fvcanquil@gmail.com>
2024-08-25 17:09:03 -0400

    Inicializando JS con Console log

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 
+console.log ("Hola, soy una prueba")
 
-
-document.querySelectorAll('.btn-outline-primary').forEach(button => {
-    button.addEventListener('click', function() {
+ //document.querySelectorAll('.btn-outline-primary').forEach(button => {
+ //    button.addEventListener('click', function() {
         // Encuentra el formulario dentro de la misma tarjeta
-        const card = this.closest('.card');
-        const form = card.querySelector('form');
+ //        const card = this.closest('.card');
+ //        const form = card.querySelector('form');
         
         // Alterna la visibilidad del formulario
-        if (form.classList.contains('hidden-form')) {
-            form.classList.remove('hidden-form');
-        } else {
-            form.classList.add('hidden-form');
-        }
-    });
-});
+  //       if (form.classList.contains('hidden-form')) {
+   //          form.classList.remove('hidden-form');
+    //     } else {
+    //         form.classList.add('hidden-form');
+ //        }
+ //    });
+ //});
 
 
 
