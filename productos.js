6eb5af0a0c822cdee99ae99e150251c2486c69f5
FranV <fvcanquil@gmail.com>
2024-08-27 16:53:52 -0400

    Modificación a como se muestra el carrito

--- productos.js
+++ productos.js
@@ -10,14 +10,12 @@ const obtenerProductos = async () => {
         mostrarProductos(data);
     } catch (error) {
         console.error('Error al obtener los datos:', error);
-        // Usar JSON local como respaldo
         try {
             let localRes = await fetch(JSON_URL);
             let localData = await localRes.json();
             mostrarProductos(localData);
         } catch (localError) {
             console.error('Error al obtener datos del archivo JSON:', localError);
-            // Mostrar un mensaje de error al usuario
             main.innerHTML = '<p>Ocurrió un error al cargar los datos. Por favor, inténtalo de nuevo más tarde.</p>';
         }
     }
@@ -30,8 +28,8 @@ const mostrarProductos = (productos) => {
         <div class='card'> 
             <h3>${e.name}</h3>
             <img src="${e.image_url}"/> 
-            <p>${e.description}</p>      
-            <p>$${e.price}</p>
+            <p class="precio">$${e.price}</p>
+            <p>${e.description}</p>                
             <button class="btnSeleccionar" onclick="agregarAlCarrito('${e._id}', '${e.name}', ${e.price}, '${e.image_url}')">Seleccionar</button>
         </div>
         `;
